9fbefcf945bfa3766a7d21d908e73eaa0e67436e
Aftermaths <fnzzhou@gmail.com>
2015-07-30 17:54:18 +0100

    more refactoring

--- lib/board.rb
+++ lib/board.rb
@@ -6,29 +6,34 @@ class Board
   attr_reader :coords_not_available
   attr_reader :ships_on_board
   attr_reader :number_of_hits
+  attr_reader :number_of_misses
 
   def initialize
     @placed_ships = {}
     @prev_targets = []
     @coords_not_available = []
     @ships_on_board = []
-    @number_of_hits = 0
-    @number_of_misses = 0
+    @number_of_hits = []
+    @number_of_misses = []
   end
 
   def place(ship, coords, direction)
     coords = coords.downcase
     off_grid(ship, coords, direction)
     coords_in_use(ship, coords, direction)
-    get_coords(ship, coords, direction).each do |coord|
-      placed_ships[coord] = ship
-    end
+    placing_ships(ship, coords, direction)
     @ships_on_board << ship
   end
 
+  def placing_ships(ship, coords, direction)
+    get_coords(ship, coords, direction).each do |coord|
+      placed_ships[coord] = ship
+    end
+  end
+
   def get_coords(ship, coord, direction)
-   coords = [coord]
-   (ship.size - 1).times { coords << (direction == :horizontal ? next_horizontal(coords.last) : next_vertical(coords.last)) }
+    coords = [coord]
+    (ship.size - 1).times { coords << (direction == :horizontal ? next_horizontal(coords.last) : next_vertical(coords.last)) }
    coords
   end
 
@@ -38,16 +43,16 @@ class Board
 
   def coords_in_use(ship, coords, direction)
     coords = get_coords(ship, coords, direction)
-    fail 'Coordinates already occupied' if coords.any? &has_ships
+    fail 'Coordinates already occupied' if coords.any?(&ships?)
   end
 
-  def has_ships
+  def ships?
     proc do |coord|
-      has_ship?(coord)
+      ship?(coord)
     end
   end
 
-  def has_ship?(coord)
+  def ship?(coord)
     placed_ships.keys.include?(coord)
   end
 
@@ -56,21 +61,21 @@ class Board
     ((coord[0].downcase <= 'j') && (coord[1..-1].to_i <= 10))
   end
 
-  def fire(coord)
-    fail 'Already fired on' if coords_already_hit?(coord)
-    @prev_targets << coord
-    placed_ships.include?(coord) ? hit(coord) : miss
+  def fire(coords)
+    fail 'Already fired on' if coords_already_hit?(coords)
+    @prev_targets << coords
+    placed_ships.include?(coords) ? hit(coords) : miss(coords)
   end
 
-  def miss
-    @number_of_misses += 1
+  def miss(coords)
+    @number_of_misses << coords
     'Miss'
   end
 
-  def hit(coord)
-    @number_of_hits += 1
-    placed_ships[coord].hit
-    placed_ships[coord].sunk? ? sunk_ship(placed_ships[coord]) : 'Hit'
+  def hit(coords)
+    @number_of_hits << coords
+    placed_ships[coords].hit
+    placed_ships[coords].sunk? ? sunk_ship(placed_ships[coords]) : 'Hit'
   end
 
   def sunk_ship(ship)
@@ -79,11 +84,11 @@ class Board
   end
 
   def victory?
-    @number_of_hits > 0 && @ships_on_board.empty?
+    @number_of_hits.count > 0 && @ships_on_board.empty?
   end
 
-  def coords_already_hit?(coord)
-    @prev_targets.any? { |a| a == coord }
+  def coords_already_hit?(coords)
+    @prev_targets.any? { |coord| coord == coords }
   end
 
   def next_horizontal(coords)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,13 +15,47 @@ describe Board do
     expect(subject.placed_ships).not_to be_empty
   end
 
+  it 'gets the next horizontal coords' do
+    expect(subject.next_horizontal(:a1)).to eq :b1
+  end
+
+  it 'gets the next vertical coords' do
+    expect(subject.next_vertical(:a1)).to eq :a2
+  end
+
+  it 'gets the next coords' do
+    expect(subject.get_coords(ship3, :a3, :horizontal)).to eq [:a3, :b3, :c3]
+  end
+
   it 'only allows placement within limits of board' do
     expect { subject.place ship3, :a12, :horizontal } .to raise_error ('Off Grid')
   end
 
-  it 'registers hits' do
+  it "won't allow ships to overlap" do
+    subject.place ship3, :a1, :horizontal
+    expect { subject.place ship3, :a1, :horizontal }.to raise_error ('Coordinates already occupied')
+  end
+
+  it 'says hit' do
     subject.place ship3, :a3, :horizontal
     expect(subject.fire :a3).to eq 'Hit'
+  end
+
+  it 'says miss' do
+    subject.place ship3, :a3, :horizontal
+    expect(subject.fire :a4).to eq 'Miss'
+  end
+
+  it 'registers hits' do
+    subject.place ship1, :a3, :horizontal
+    subject.fire :a3
+    expect(subject.number_of_hits).to eq [:a3]
+  end
+
+  it 'registers misses' do
+    subject.place ship1, :a3, :horizontal
+    subject.fire :j1
+    expect(subject.number_of_misses).to eq [:j1]
   end
 
   it "won't allow hits on same spot twice" do
@@ -30,22 +64,13 @@ describe Board do
     expect { subject.fire :a3 }.to raise_error ('Already fired on')
   end
 
-  it 'gets the next horizontal coords' do
-    expect(subject.get_coords(ship3, :a1, :horizontal)).to eq [:a1, :b1, :c1]
-  end
-
-  it "won't allow a ship to be overwritten" do
-    subject.place ship3, :a1, :horizontal
-    expect { subject.place ship3, :a1, :horizontal }.to raise_error ('Coordinates already occupied')
-  end
-
   it "knows if a coord doesn't have a ship on it" do
-    expect(subject.has_ship? :a1).to eq false
+    expect(subject.ship? :a1).to eq false
   end
 
   it 'knows if a coord has a ship on it' do
     allow(subject).to receive(:placed_ships).and_return({ :a1 => ship1 })
-    expect(subject.has_ship? :a1).to eq true
+    expect(subject.ship? :a1).to eq true
   end
 
   it 'says you\'ve sunk a ship' do
@@ -63,5 +88,11 @@ describe Board do
     allow(ship1).to receive(:sunk?).and_return(true)
     subject.fire :a1
     expect(subject.victory?).to eq true
+  end
+
+  it 'correctly deletes ship from board' do
+    allow(ship1).to receive(:ships_on_board).and_return(ship1)
+    subject.sunk_ship ship1
+    expect(subject.ships_on_board).to be_empty
   end
 end
